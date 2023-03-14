a3ee32562d7476eed3683855df3e3af412d45743
Dmitry <mr.dima.alexeev@gmail.com>
2023-03-14 20:12:04 +0300

    aaaaa

--- lab1/main.py
+++ lab1/main.py
@@ -1,10 +1,40 @@
 from methods import *
 
+
 def func1(x: float) -> float:
-    return 3*x - x**3 -1
+    return x**3/3 - 2*x**2 + 3*x + 5/3
 
-def func2(x:float) -> float:
-    return -3*x**2 + 7*x + 2
 
-Dichotomic_search(func1, -3, 0, 0.01, 0.1)
-Golden_search(func1, -3, 0, 0.01, 0.1)
+def func2(x: float) -> float:
+    return (x**2 - 2*x - 3) / (x**4 - 5*x**2 + 4)
+
+
+result_files = {
+    "Dichotomic_search": "dich.txt",
+    "Golden_search": "golden_search.txt",
+    "Fibonacci": "fib.txt"
+}
+
+# функция 1
+x_dich, res_dich = Dichotomic_search(func1, -2, 2, 0.01, 0.1)
+x_gold, res_gold = Golden_search(func1, -1, 10, 0.1)
+x_fib, res_fib = Fibonacci_search(func1, -1, 10, 0.01, 0.1)
+with open('dich_min.txt', 'w') as f_d,\
+     open('gold_min.txt', 'w') as f_g,\
+     open('fib_min.txt', 'w')  as f_b:
+
+    f_d.write(f'x* = {x_dich}\n'+res_dich)
+    f_g.write(f'x* = {x_gold}\n'+res_gold)
+    f_b.write(f'x* = {x_fib}\n'+res_fib)
+
+# функция 2
+x_dich, res_dich = Dichotomic_search(func2, -1, 10, 0.01, 0.1)
+x_gold, res_gold = Golden_search(func2, -1, 10, 0.1)
+x_fib, res_fib = Fibonacci_search(func2, -1, 10, 0.01, 0.1)
+with open('dich_max.txt', 'w') as f_d,\
+     open('gold_max.txt', 'w') as f_g,\
+     open('fib_max.txt', 'w')  as f_b:
+
+    f_d.write(f'x* = {x_dich}\n'+res_dich)
+    f_g.write(f'x* = {x_gold}\n'+res_gold)
+    f_b.write(f'x* = {x_fib}\n'+res_fib)
--- lab1/methods.py
+++ lab1/methods.py
@@ -1,21 +1,15 @@
 from prettytable import PrettyTable
 
-result_files = {
-    "Dichotomic_search": "dich.txt",
-    "Golden_search": "golden_search.txt",
-    "Fibonacci": "fib.txt"
-}
 
-
-def Dichotomic_search(func, a: float, b: float, eps: float, l: float) -> None:
+def Dichotomic_search(func, a: float, b: float, eps: float, l: float) -> tuple[float, str]:
     """Dichotomic search method"""
-    
+
     result = PrettyTable()
-    result.field_names = ["k","a(k)", "b(k)",
+    result.field_names = ["k", "a(k)", "b(k)",
                           "lmbd(k)", "mu(k)",
                           "F(lmbd(k))", "F(mu(k))"
                           ]
-    
+
     k = 1
     while b - a > l:
 
@@ -35,53 +29,108 @@ def Dichotomic_search(func, a: float, b: float, eps: float, l: float) -> None:
         k += 1
 
     result.add_row([k, a, b, lmbd, mu, func_lmbd, func_mu])
-    f = open(result_files["Dichotomic_search"], 'w')
-    f.write(result.get_string())
-    f.write(f'\nx* = {(a + b)/2}')
-    f.close()
+
+    return (a + b)/2, result.get_string()
 
 
-def Golden_search(func, a: float, b: float, eps: float, l: float) -> None:
+def Golden_search(func, a: float, b: float, l: float) -> tuple[float, str]:
     """Golden-section search method"""
 
-    alpha = (5 ** 0.5 - 1) / 2
-
     result = PrettyTable()
-    result.field_names = ["k","a(k)", "b(k)",
+    result.field_names = ["k", "a(k)", "b(k)",
                           "lmbd(k)", "mu(k)",
                           "F(lmbd(k))", "F(mu(k))"
                           ]
-            
+
+    alpha = (5 ** 0.5 + 1) / 2
     k = 1
-    lmbd = a + (1 - alpha) * (b - a)
-    mu = a + alpha * (b - a)
+    lmbd = a + (b - a)/(alpha + 1)
+    mu = b - (b-a)/(alpha + 1)
     func_lmbd = func(lmbd)
     func_mu = func(mu)
     result.add_row([k, a, b, lmbd, mu, func_lmbd, func_mu])
 
     while b - a > l:
-        
-        #result.add_row([k, a, b, lmbd, mu, func_lmbd, func_mu])
-        
+
         if func_lmbd > func_mu:
             a = lmbd
             lmbd = mu
-            mu = a + alpha * (b - a)
+            func_mu = func_lmbd
+            mu = b - (b - a)/(alpha + 1)
             func_mu = func(mu)
-            func_lmbd = func(lmbd)
         else:
             b = mu
             mu = lmbd
-            lmbd = a + (1 - alpha) * (b - a)
+            func_mu = func_lmbd
+            lmbd = a + (b - a)/(alpha + 1)
             func_lmbd = func(lmbd)
-            func_mu = func(mu)
         k += 1
         result.add_row([k, a, b, lmbd, mu, func_lmbd, func_mu])
-        
+
+    return (a + b)/2, result.get_string()
+
+
+def Fibonacci_search(func, a: float, b: float, eps: float, l: float) -> tuple[float, str]:
+    """Fibonacci search method"""
+
+    result = PrettyTable()
+    result.field_names = ["k", "a(k)", "b(k)",
+                          "lmbd(k)", "mu(k)",
+                          "F(lmbd(k))", "F(mu(k))"
+                          ]
+
+    fibs = [1, 1]
+    n = 2
+    while True:
+        fib = fibs[n-1]+fibs[n-2]
+        fibs.append(fib)
+        if fib > (b - a)/l:
+            break
+        n += 1
+
+    k = 1
+    lmbd = a + fibs[n-2]/fibs[n]*(b-a)
+    mu = a + fibs[n-1]/fibs[n]*(b-a)
+    func_lmbd = func(lmbd)
+    func_mu = func(mu)
     result.add_row([k, a, b, lmbd, mu, func_lmbd, func_mu])
 
-    f = open(result_files["Golden_search"], 'w')
-    f.write(result.get_string())
-    f.write(f'\nx* = {(a + b)/2}')
-    f.close()
+    while b - a > l:
+        # первый шаг
+        if func(lmbd) > func(mu):
+            # второй шаг
+            a = lmbd
+            lmbd = mu
+            mu = a + fibs[n-k-1]/fibs[n-k]*(b-a)
+            if k == n - 2:
+                # пятый шаг
+                mu = lmbd + eps
+                if func(lmbd) > func(mu):
+                    a = lmbd
+                else:
+                    b = mu
+                result.add_row([k+1, a, b, lmbd, mu, func_lmbd, func_mu])
+                break
+            else:
+                func_mu = func(mu)
+        else:
+            # третий шаг
+            b = mu
+            mu = lmbd
+            lmbd = a + fibs[n-k-2]/fibs[n-k]*(b-a)
+            if k == n - 2:
+                # 5 шаг
+                mu = lmbd + eps
+                if func(lmbd) > func(mu):
+                    a = lmbd
+                else:
+                    b = mu
+                result.add_row([k+1, a, b, lmbd, mu, func_lmbd, func_mu])
+                break
+            else:
+                func_lmbd = func(lmbd)
+        # 4 шаг
+        k += 1
+        result.add_row([k, a, b, lmbd, mu, func_lmbd, func_mu])
 
+    return (a + b)/2, result.get_string()
